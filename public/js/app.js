8338373a0770714a5d4565001aefc33cc5e73278
Alex Jones <alex.l.jones.3@gmail.com>
2016-09-28 20:22:00 -0400

    added the ability for the minutes to cycle based on seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 (function() {
   //initialize variables
-  var startButton =$('#start');
+  var startButton = $('#start');
   var seconds = $('#seconds');
+  var minutes = $('#minutes');
   //main functionality
   startButton.on('click', countdown);
 
@@ -9,21 +10,32 @@
   function countdown(){
     var secondsText = seconds.text();
     var secondsTextAsNumber = parseInt(secondsText);
+    var minutesText = minutes.text();
+    var minutesTextAsNumber = parseInt(minutesText);
     //console.log(typeof secondsText);
     //console.log(typeof secondsTextAsNumber);
+    if(minutesTextAsNumber === 0 && secondsTextAsNumber === 0){
+      //stop
+      return; //temporary
+    }
     if(secondsTextAsNumber === 0) {
+      if(minutesTextAsNumber !== 0){
+        var decreasedMinutesAsNumberByOne = minutesTextAsNumber - 1;
+        var padMinutesTextAsNumber = pad(decreasedMinutesAsNumberByOne);
+        minutes.text(padMinutesTextAsNumber);
+      }
       seconds.text("59");
-
+      //then change seconds text to 59
     } else {
       var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
-      var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne)
+      var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
       seconds.text(padSecondsTextAsNumber);
     }
-    
-    }//var secondsValue = parseInt(seconds.text());
 
+    //var secondsValue = parseInt(seconds.text());
+    //
     //seconds.text(pad(secondsValue - 1));
-
+   }
 
   function pad(num){
     if(num < 10){
